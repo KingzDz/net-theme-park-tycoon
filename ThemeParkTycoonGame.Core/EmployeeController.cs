40225088c5a9f952d534d39d77583e90c6f32cbb
Evilman <timo02@me.com>
2019-11-14 12:43:41 +0100

    Added EmployeeController

---
+++ ThemeParkTycoonGame.Core/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThemeParkTycoonGame.Core
+{
+    class EmployeeController
+    {
+        List<Employee> WorkerList = new List<Employee>();
+
+        public EmployeeController()
+        {
+            WorkerList.Add(NewEmployee());
+
+        }
+
+        public Employee NewEmployee()
+        {
+            Employee worker = new Employee();
+            worker.Function = "Performer";
+            worker.Boost.StatType = StatTypes.GetByUniqueId("excitement");
+            return worker;
+        }
+    }
+}
