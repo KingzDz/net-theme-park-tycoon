6bac7ce5c53d04dbf57f9a9722d97384507aa7b4
Aj <44425998+DarkDemonCatX256@users.noreply.github.com>
2019-11-21 11:11:57 +0100

    Added edit guests button (causes crash) needs work

--- ThemeParkTycoonGame.Fancy/Controls/DebugView.xaml.cs
+++ ThemeParkTycoonGame.Fancy/Controls/DebugView.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using ThemeParkTycoonGame.Fancy;
 namespace ThemeParkTycoonGame.Fancy.Controls
 {
     /// <summary>
@@ -20,9 +20,19 @@ namespace ThemeParkTycoonGame.Fancy.Controls
     /// </summary>
     public partial class DebugView : UserControl
     {
+        GuestsView guests;
         public DebugView()
         {
+           
             InitializeComponent();
+        }
+
+        private void EditGuestButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(guests.guestsListView.Items.Count > 0)
+            {
+                MessageBox.Show(guests.guestsListView.Items[0].ToString());
+            }
         }
     }
 }
