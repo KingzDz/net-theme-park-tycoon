47971473c4b6e0cefa4be6054583e6d48edcbae0
Unknown <enevik@hotmail.com>
2019-11-28 12:38:46 +0100

    weatherchange

--- ThemeParkTycoonGame.Fancy/Controls/weatherChange.xaml.cs
+++ ThemeParkTycoonGame.Fancy/Controls/weatherChange.xaml.cs
@@ -26,7 +26,12 @@ namespace ThemeParkTycoonGame.Fancy.Controls
 
         private void WeatherChangeButton_Click(object sender, RoutedEventArgs e)
         {
-            string[] weathers = { "rain", "storm", "sun" };
+            string[] rain = { "rain", "/Resources/weather_rain_48.png" };
+            string[] storm = { "storm", "/Resources/weather_storm_48.png" };
+            string[] snow = { "snow", "/Resources/weather_snow_48.png" };
+            string[] sun = { "sun", "Resources/weather_sun_48.png" };
+            string[] weathers = { rain[0], storm[0], sun[0], snow[0] };
+
             Random rand = new Random();
             int index = rand.Next(weathers.Length);
 
