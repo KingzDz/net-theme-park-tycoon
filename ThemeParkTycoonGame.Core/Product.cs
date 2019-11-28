d729667b8999f50e8e411b28c684cbbb076cedb1
Stijn van Dongen <Stijn_vandongen@outlook.com>
2019-11-28 12:42:48 +0100

    Product page

--- ThemeParkTycoonGame.Core/Product.cs
+++ ThemeParkTycoonGame.Core/Product.cs
@@ -5,5 +5,22 @@
         public string Name { get; }
         public string Description { get; }
         public decimal Price { get; }
+
+        public Product()
+        {
+            Name = "untitled Product";
+            Description = "New Product";
+            Price = 0;
+
+        }
+
+        public Product(string name, string description, decimal price)
+            : this()
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+        }
+
     }
 }
--- ThemeParkTycoonGame.Fancy/Controls/BuyShop.cs
+++ ThemeParkTycoonGame.Fancy/Controls/BuyShop.cs
@@ -1,15 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 
 public class Shop
 {
-    public string Image { get; set; }
+    // public string Image { get; set; }
     public string Name { get; set; }
-    public 
     public string Description { get; set; }
     public int Price { get; set; }
-   
 
+    public override string ToString()
+    {
+        return string.Format("{0}  {1} (kost: {2})", Name, Description, Price);
+    }
 
+    public static List<product> getAll()
+    {
+        return new List<product>()
+        {
+            new product()
+            {
+                Name = "Hamburger",
+                Description = "Hamburger met Sla, Tomaat en augurk",
+                Price = "5,-"
+                
+            },
+        };
+
+    }
 
 }
